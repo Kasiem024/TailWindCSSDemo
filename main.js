63e756733c0c96f05c915ea6fc1734feb2aa7ca6
Kasiem Saeed <kasiemsaeed@gmail.com>
2022-05-27 15:36:09 +0200

    testing env variables

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
 'use strict';
 
 console.log('main.js is alive');
+
+window.onload = () => {
+    console.log('Hello World');
+    let post = 'This is a post';
+    fetch('http://localhost:8888/.netlify/functions/hello-world', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                post: post
+            })
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        });
+};
--- netlify/functions/hello-world/hello-world.js
+++ netlify/functions/hello-world/hello-world.js
@@ -1,17 +1,21 @@
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
-const handler = async (event) => {
-  try {
-    const subject = event.queryStringParameters.name || 'World'
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
+require('dotenv').config()
+const handler = async(event) => {
+    try {
+        console.log(JSON.parse(event.body).post);
+        console.log(process.env.TEST);
+
+        const subject = JSON.parse(event.body).post || 'World'
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: `Hello ${subject}` }),
+            // // more keys you can return:
+            // headers: { "headerName": "headerValue", ... },
+            // isBase64Encoded: true,
+        }
+    } catch (error) {
+        return { statusCode: 500, body: error.toString() }
     }
-  } catch (error) {
-    return { statusCode: 500, body: error.toString() }
-  }
 }
 
 module.exports = { handler }
